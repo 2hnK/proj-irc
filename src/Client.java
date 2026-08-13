6a82459e348781178a5acc71f31d4ff17884dfb7
Kim Jihun <kimgh921@gmail.com>
2026-08-13 17:08:27 +0900

    refactor: 서버 진입점 클래스명을 IrcServer로 변경

--- src/Client.java
+++ src/Client.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class Client {
+    
     private static final String SERVER_ADDRESS = "127.0.0.1";
     private static final int SERVER_PORT = 9910;
 
--- src/IrcServer.java
+++ src/IrcServer.java
@@ -2,20 +2,21 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
 
-public class MainController {
+public class IrcServer {
+
     private static final int SERVER_PORT = 9910;
 
     public static void main(String[] args) {
         ChannelManager channelManager = new ChannelManager();
-        
+
         try (ServerSocket serverSocket = new ServerSocket(SERVER_PORT)) {
             System.out.println("IRC Server started on port " + SERVER_PORT);
-            
+
             while (true) {
                 try {
                     Socket clientSocket = serverSocket.accept();
                     System.out.println("New client connected: " + clientSocket.getInetAddress());
-                    
+
                     startClientThread(clientSocket, channelManager);
                 } catch (IOException e) {
                     System.err.println("Error accepting client connection: " + e.getMessage());
